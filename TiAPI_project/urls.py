e514d8a2ca91e8e9b7513ceaac07731674732fcc
KundaPanda <vojdoh@gmail.com>
2019-10-21 21:56:18 +0200

    Added default empty path.

--- TiAPI_project/urls.py
+++ TiAPI_project/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('add/code', AddCode.as_view()),
     path('add/user', AddUser.as_view()),
     path('get/users', GetUsers.as_view()),
